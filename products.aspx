576616592bcc5d339d15e143a59a0613faa4cc60
HyperChrisX <cj55448@hotmail.com>
2015-11-30 13:16:31 -0600

    Added data grid, currently broken...

--- products.aspx
+++ products.aspx
@@ -11,7 +11,23 @@
 <body>
 <div>
     <uc:navigation ID="navigation" runat="server" /> <!-- navigation -->
-    <h1>Our Products</h1> <!-- title of the whole page -->
+    <form id="form1" runat="server">
+    <div>
+      <h1>Our Products</h1> <!-- title of the whole page -->
+      SEARCH: <asp:TextBox ID="searchTerm" runat="server" />
+      <asp:Button ID="searchBtn" runat="server"
+        Text="Search" OnClick="searchBtn_OnClick" />
+        <br /> By Default this search will return all matches to your terms.
+      <br />
+      <asp:Label ID="statusL" runat="server" />
+      
+      
+      <asp:GridView ID="ProductsGrid" runat="server" AllowSorting="true" OnSortCommand="dgSearchList_SortClick" >
+      
+      </asp:GridView>
+     
+      </div>
+    </form>
 </div>
 </body>
 </html>
--- products.aspx.cs
+++ products.aspx.cs
@@ -4,11 +4,117 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
 
-public partial class products : System.Web.UI.Page
+public partial class DataGrid_Products : System.Web.UI.Page
 {
+
     protected void Page_Load(object sender, EventArgs e)
     {
+        if (!Page.IsPostBack)
+        {
+            PopulateDatagrid();
+        }
+    }
+    protected void searchBtn_OnClick(object sender, EventArgs e)
+    {
+        try
+        {
+            string connectionString
+               = ConfigurationManager.ConnectionStrings["AdventureWorks2012"].ConnectionString;
+
+            statusL.Text = "Searching for " + searchTerm.Text;
+            string sqlQuery = "SELECT  ProductID,Name,ProductNumber, Color " +
+                               " FROM Production.Product " +
+                               " Where 1=2";
+
+
+            string[] searchTerms = searchTerm.Text.Replace(';', ' ').Replace("'", "").Split(' ');
+
+            foreach (string term in searchTerms)
+            {
+                sqlQuery += " OR Name like '%" + term + "%' ";
+            }
+            SqlDataAdapter outlookRecords =
+                    new SqlDataAdapter(sqlQuery, connectionString);
+
+            // Create and fill a DataSet.
+            DataSet ds = new DataSet();
+            outlookRecords.Fill(ds);
+            DataView dv = new DataView(ds.Tables[0]);
+            ProductsGrid.DataSource = dv;
+            ProductsGrid.DataBind();
+        }
+        catch (Exception exc)
+        {
+            statusL.Text = exc.Message;
+        }
+
 
     }
+    //public void dgSearchList_SortClick(object sender, DataGridSortCommandEventArgs e)
+    //{
+    //    // ie the Sortexpression assigned to the Column.Check STEP III for how to assign a   // sortexpression on a column.
+    //    PopulateDatagrid(e.SortExpression); //Call the method that populates the Datagrid with
+    //    //the values from the Dataview.
+    //}
+    //public void PopulateDatagrid(string sortField)
+    //{
+    //    try
+    //    {
+
+    //        statusL.Text = "This should be sorted";
+    //        string connectionString
+    //            = ConfigurationManager.ConnectionStrings["AdventureWorks2012"].ConnectionString;
+    //        string sqlQuery = "SELECT ProductID,Name,ProductNumber, Color " +
+    //                          " FROM Production.Product; ";
+    //        SqlDataAdapter outlookRecords =
+    //            new SqlDataAdapter(sqlQuery, connectionString);
+
+    //        // Create and fill a DataSet.
+    //        DataSet ds = new DataSet();
+    //        outlookRecords.Fill(ds);
+    //        DataView dv = new DataView(ds.Tables[0]);
+    //        dv.Sort = sortField;
+    //        ProductsGrid.DataSource = dv; 
+    //        ProductsGrid.DataBind();
+
+    //    }
+    //    catch (Exception exc)
+    //    {
+
+    //        statusL.Text = exc.Message;
+    //    }
+
+    //}
+    public void PopulateDatagrid()
+    {
+        try
+        {
+            statusL.Text += "This should not be sorted";
+            string connectionString
+               = ConfigurationManager.ConnectionStrings["AdventureWorks2012"].ConnectionString;
+            string sqlQuery = "SELECT ProductID,Name,ProductNumber, Color " +
+                              " FROM Production.Product; ";
+            SqlDataAdapter outlookRecords =
+                new SqlDataAdapter(sqlQuery, connectionString);
+
+            // Create and fill a DataSet.
+            DataSet ds = new DataSet();
+            outlookRecords.Fill(ds);
+            DataView dv = new DataView(ds.Tables[0]);
+            ProductsGrid.DataSource = dv;
+            ProductsGrid.DataBind();
+
+        }
+        catch (Exception exc)
+        {
+
+            statusL.Text = exc.Message;
+        }
+
+    }
+
 }
