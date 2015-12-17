f720ac0dc4d4f2858994b3fa3e310df5cc76da3a
HyperChrisX <cj55448@hotmail.com>
2015-12-17 13:24:29 -0600

    ...

--- shopping.aspx.cs
+++ shopping.aspx.cs
@@ -22,16 +22,16 @@ public partial class shoppingCart : System.Web.UI.Page
                     curCart.remItem(Request.QueryString["productID"]);
                     Response.Redirect("shopping.aspx", false);
                 }
-                else if (Request.QueryString["operation"].Equals("emptyCart"))
-                {
-                    curCart.emptyCart(Request.QueryString["productID"]);
-                    Response.Redirect("shopping.aspx");
-                }
-                else if (Request.QueryString["operation"].Equals(null))
-                {
-                    curCart.showCart();
-                    CartBody.Text += curCart.showCart();
-                }
+                //else if (Request.QueryString["operation"].Equals("emptyCart"))
+                //{
+                //    curCart.emptyCart(Request.QueryString["productID"]);
+                //    Response.Redirect("shopping.aspx");
+                //}
+                //else if (Request.QueryString["operation"].Equals(null))
+                //{
+                //    curCart.showCart();
+                //    CartBody.Text += curCart.showCart();
+                //}
             }
             else if (Session["savedCart"] == null)
             {
